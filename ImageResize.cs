2a835e94b7512936eb831779892673204f9756fa
devshark <ast.lim12@gmail.com>
2012-11-19 11:24:47 +0800

    Initial feature : creating thumbnails from images in the current directory

---
+++ ImageResize.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace ImageResizer
+{
+    class ImageResize
+    {
+        public static void ResizeImage(string OriginalFile, string NewFile, int NewWidth, int MaxHeight, bool OnlyResizeIfWider=false)
+        {
+            System.Drawing.Image FullsizeImage = System.Drawing.Image.FromFile(OriginalFile);
+
+            // Prevent using images internal thumbnail
+            FullsizeImage.RotateFlip(System.Drawing.RotateFlipType.Rotate180FlipNone);
+            FullsizeImage.RotateFlip(System.Drawing.RotateFlipType.Rotate180FlipNone);
+
+            if (OnlyResizeIfWider)
+            {
+                if (FullsizeImage.Width <= NewWidth)
+                {
+                    NewWidth = FullsizeImage.Width;
+                }
+            }
+
+            int NewHeight = FullsizeImage.Height * NewWidth / FullsizeImage.Width;
+            if (NewHeight > MaxHeight)
+            {
+                // Resize with height instead
+                NewWidth = FullsizeImage.Width * MaxHeight / FullsizeImage.Height;
+                NewHeight = MaxHeight;
+            }
+
+            System.Drawing.Image NewImage = FullsizeImage.GetThumbnailImage(NewWidth, NewHeight, null, IntPtr.Zero);
+
+            // Clear handle to original file so that we can overwrite it if necessary
+            FullsizeImage.Dispose();
+
+            // Save resized picture
+            NewImage.Save(NewFile);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ImageResizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                (new Program()).start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Main(args);
+            }
+        }
+
+        public void start()
+        {
+            Console.Write("Enter the desired with height > ");
+            String strHeight = Console.ReadLine();
+            Console.Write("Enter the desired with width > ");
+            String strWidth = Console.ReadLine();
+            try
+            {
+                Int32 intHeight = Convert.ToInt32(strHeight);
+                Int32 intWidth = Convert.ToInt32(strWidth);
+
+                String imagePath = Directory.GetCurrentDirectory();
+                Console.WriteLine(imagePath);
+                String thumbnailPath = Path.Combine(imagePath, "thumbs");
+
+                if (!Directory.Exists(thumbnailPath))
+                {
+                    Directory.CreateDirectory(thumbnailPath);
+                }
+
+                foreach (string image in Directory.GetFiles(imagePath, "*.jpg"))
+                {
+                    ImageResize.ResizeImage(imagePath, thumbnailPath, intWidth, intHeight);
+                }
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
